7436f57ee244a71c267fdc167f735a4d0b3c203f
Edoardo Tronconi <edoardotronconi97@gmail.com>
2021-02-09 14:23:34 +0100

    piccola modifica heapsort

--- c++/heapsort.cpp
+++ c++/heapsort.cpp
@@ -13,10 +13,9 @@ void max_heapify(const Iterator start, const Iterator end, Iterator i){
     Iterator leftChild = std::next(start, 2 * (std::distance(start, i) + 1 ) - 1);
     Iterator rightChild = std::next(start, 2 * (std::distance(start, i) + 1 ) );
     
-    Iterator largest;
+    Iterator largest = i;
     
     if ((std::distance(leftChild,end) > 0) and (*leftChild > *i)) largest = leftChild;
-    else largest = i;
     if ((std::distance(rightChild, end) > 0) and (*rightChild > *largest) ) largest = rightChild;
     
     if (largest != i){
